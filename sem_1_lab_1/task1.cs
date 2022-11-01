f225faed878b33b5625050b01b340c513fc5ba96
Oleksii Kovalenko <alex59.kovalenko@gmail.com>
2022-11-01 10:33:13 +0200

    Повна реалізація завдань 1 та 2

--- sem_1_lab_1/task1.cs
+++ sem_1_lab_1/task1.cs
@@ -11,6 +11,23 @@ namespace Program
              * case 4 x = 1 y = 1 z = 124
              */
             double x, y, z, a, b;
+            Console.WriteLine("Enter value for x: ");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter value for y: ");
+            y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter value for z: ");
+            z = Convert.ToDouble(Console.ReadLine());
+            if (y == 0 || Math.Pow(x, 3) + 5 * Math.Pow(y, -z) + Math.Pow(z, 2) <= 0 || y == Math.Pow(Math.Abs(Math.Pow(x, 3)), 0.5))
+            {
+                Console.WriteLine("Помилка");
+            }
+            else
+            {
+                a = (y - Math.Pow(Math.Abs(Math.Pow(x, 3)), 0.5)) / Math.Pow((Math.Pow(x, 3) + 5 * Math.Pow(y, -z) + Math.Pow(z, 2)), 0.5);
+                b = Math.Sin(Math.Pow(a, -x)) + y;
+                Console.WriteLine("a = " + Math.Round(a, 4));
+                Console.WriteLine("b = " + Math.Round(b, 4));
+            }
             /* case 1 a = 0.3068 b = 1.8823
              * case 2 a = -0.3898 b = 1.2931
              * case 3 "Помилка"
--- sem_1_lab_1/task2.cs
+++ sem_1_lab_1/task2.cs
@@ -15,6 +15,38 @@ namespace Program
             int counter1 = 0; // лічильник для перевірки чи просте число i
             int counter2 = 0; // лічильник для перевірки чи просте число m
             List<int> nums = new(); // список для знайдених чисел Мерсенна
+            Console.WriteLine("Enter value for n: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i < n; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        counter1++;
+                    }
+                }
+                if (counter1 == 2)
+                {
+                    m = Math.Log(i + 1, 2);
+                    if (m == Convert.ToInt32(m))
+                    {
+                        for (int k = 1; k <= m; k++)
+                        {
+                            if (m % k == 0)
+                            {
+                                counter2++;
+                            }
+                        }
+                        if (counter2 == 2)
+                        {
+                            nums.Add(i);
+                        }
+                    }
+                    counter2 = 0;
+                }
+                counter1 = 0;
+            }
             for (int p = 0; p < nums.Count(); p++)
             {
                 Console.Write(nums[p] + " ");
