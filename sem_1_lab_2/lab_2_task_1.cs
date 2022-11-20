749f0597253d3337041ec7218f8d24469c5a6cb9
Oleksii Kovalenko <alex59.kovalenko@gmail.com>
2022-11-20 17:51:35 +0200

    Додано тестовий приклад

--- sem_1_lab_2/lab_2_task_1.cs
+++ sem_1_lab_2/lab_2_task_1.cs
@@ -7,19 +7,32 @@ namespace Program
         {
             int n = 0; //задана користувачем кількість строк 
             int m = 0; //задана користувачем кількість стовпців
-            int range; //задане користувачем максимальне число у матриці
+            int range = 0; //задане користувачем максимальне число у матриці
             int temp; //змінна для визначення кількості розрядів у чисел для вирівнення матриці
             int counter = 0; //лічильник для кількості розрядів максимального числа
             int counter2 = 0; //лічильник для кількості розрядів кожного нового числа в матриці
-            int lower; //змінна, що буде набувати значення мінімального числа під час руху зліва направо
+            int lower = 0; //змінна, що буде набувати значення мінімального числа під час руху зліва направо
             string lowerword = ""; //змінна, що буде мати вигляд x[i][j], де х - мінімальне число під час руху зліва направо, i та j - індекси цього числа
             List<string> lowers = new(); //список для змінних lower
             int[,] matrix = new int[n, m];
-            for (int i = 0; i < n; i++)
+            string answer;
+            string answerrow = "";
+            Console.WriteLine("Do you want to check test? Write 'yes' to start it");
+            answer = Convert.ToString(Console.ReadLine());
+            if (answer == "yes")
             {
-                for (int j = 0; j < m; j++)
+                lower = 10;
+                n = 3;
+                m = 3;
+                Console.WriteLine("n = 3, m = 3");
+                matrix = new int[3, 3] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+                Console.WriteLine("matrix = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } }");
+            }
+            if (answer == "yes")
+            {
+                if (answerrow == "3 2 1 4 7 8 9 6 5 " && lowers[0] == "7[2][0]")
                 {
-                    Console.WriteLine(matrix[i, j]);
+                    Console.WriteLine("Test is PASSED");
                 }
             }
         }
