fe969f4448612aaca58857981339b16187f7c96a
Oleksii Kovalenko <alex59.kovalenko@gmail.com>
2023-03-02 12:35:14 +0200

    прототип програми на рівні заглушок

---
+++ sem_2_lab_1/sem_2_lab_1_task_1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace Laboratory
+{
+    class Laboratory1
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new();
+            int N = 0;
+            int M = 0;
+            int K = 0;
+            int maximumValue = 0;
+            string answer;
+        }
+        static int enterData(int x, string symbol)
+        {
+            return x;
+        }
+        static int[,] specialSort(int[,] array, int n, int m, int K)
+        {
+            // список для чисел, що пройшли умову для сортування
+            List<int> searchedNumbers = new();
+
+            return array;
+        }
+        static int binarySearch(List<int> givenList, int item, int left, int right)
+        {
+            return 0;
+        }
+        static void insertionSort(List<int> givenList)
+        {
+            int j;
+            int searchedOrder;
+            int key;
+        }
+    }
+}
